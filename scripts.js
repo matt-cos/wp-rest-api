c61b18617f99c4d769328e31026bb901b1226657
Matt Cosentino <matt@learnedmedia.com>
2018-10-23 23:14:34 -0400

    combined the 2 vue apps into 1. working on getting the routing down. page routes are working, but need to put the dynamically generated content into appropariate compnents (home, about, portfolio, etc.)

--- scripts.js
+++ scripts.js
@@ -36,9 +36,9 @@ request.onload = function(){
 
 	if (request.status >= 200 && request.status < 400) {
 		data.forEach(post => {
-			wpOutput = '<div class="row post postid-' + post.id + '"><div class="column column-33 title__wrapper"><a href="file:///Users/mattcos/Development/wp-rest-api/portfolio-item/' + post.slug + '" class="title" data-bg-img="' + post._embedded['wp:featuredmedia']['0'].source_url + '">' + post.title.rendered + '</a></div></div>';
+			wpOutput = '<div class="row post postid-' + post.id + '"><div class="column column-33 title__wrapper"><a href="file:///Users/mattcos/Development/wp-rest-api/index.html#/portfolio/' + post.slug + '" class="title" data-bg-img="' + post._embedded['wp:featuredmedia']['0'].source_url + '">' + post.title.rendered + '</a></div></div>';
 
-			app4.todos.push({ text: wpOutput });
+			app.todos.push({ text: wpOutput });
 
 			// https://router.vuejs.org/guide/essentials/dynamic-matching.html
 		});
@@ -48,43 +48,62 @@ request.onload = function(){
 
 }
 
-var app4 = new Vue({
-	el: '#vue-app',
-	data: {
-		todos: [
-			// { text: 'Learn JavaScript' },
-			// { text: 'Learn Vue' },
-			// { text: 'Build something awesome' }
-		]
-	}
-});
-
 request.send();
 
 request.addEventListener("load", transferComplete);
 
+// Define a new component called button-counter
+Vue.component('button-counter', {
+	data: function () {
+		return {
+			count: 0
+		}
+	},
+	template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
 
+Vue.component('post-list', {
+	template: '<span v-for="todo in todos" v-html="todo.text"></span>'
+})
 
-
-// const app = document.getElementById('root');
-
-// const container = document.createElement('div');
-// container.setAttribute('class', 'container');
-
-// app.appendChild(container);
-
-
-
+Vue.component('blog-post', {
+	props: ['title'],
+	template: '<h3>{{ title }}</h3>'
+})
 
 
 // https://medium.com/frontend-fun/vuejs-routing-with-vue-router-1548f94c0575
-const Foo = { template: '<h1>FOO</h1>'};
-const About = { template: '<h1>ABOUT</h1>'};
+
+// useful error message:
+// [Vue warn]: Error compiling template:
+// <h3>Home Content</h3><br><button-counter></button-counter>
+// - Component template should contain exactly one root element. If you are using v-if on multiple elements, use v-else-if to chain them instead.
+
+
+
+// const Home = { template: '<h3>Home Content</h3>'};
+// const Home = { template: '<h3>Home Content</h3><br><button-counter></button-counter>'};
+const Home = { template: '<button-counter></button-counter>'};
+// const Home = { template: '<post-list></post-list>'};
+// const Home = { template: '<blog-post v-for="todo in todos" v-bind:key="todo.id" v-bind:title="todo.text"></blog-post>'};
+const Foo = { template: '<h3>Foo</h3>'};
+const About = { template: '<h3>About</h3>'};
+const User = {
+	props: ['name'],
+	template: `<h3>User: {{ name }}</h3>`
+};
+const Portfolio = {
+	props: ['name'],
+	template: `<h3>Portfolio Item: {{ name }}</h3>`
+};
 
 const routes = [
+	{ path: '/', component: Home },
 	{ path: '/foo', component: Foo },
-	{ path: '/bar', redirect: '/foo' },
+	{ path: '/bar', redirect: '/' },
 	{ path: '/about', component: About },
+	{ path: '/user/:name', component: User, props: true },
+	{ path: '/portfolio/:name', component: Portfolio, props: true },
 ];
 
 const router = new VueRouter({
@@ -93,5 +112,17 @@ const router = new VueRouter({
 })
 
 const app = new Vue({
+	data: {
+		todos: [
+			// { text: 'Learn JavaScript' },
+			// { text: 'Learn Vue' },
+			// { text: 'Build something awesome' }
+		],
+		posts: [
+			{ id: 1, title: 'My journey with Vue' },
+			{ id: 2, title: 'Blogging with Vue' },
+			{ id: 3, title: 'Why Vue is so fun' }
+		]
+	},
 	router
 }).$mount('#app');
